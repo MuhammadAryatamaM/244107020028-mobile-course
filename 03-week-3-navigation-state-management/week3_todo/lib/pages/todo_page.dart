e405f099abdd09b4a0517c9371848b3611489072
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2026-09-13 12:18:43 +0700

    refactor: pisahkan baris todo menjadi baris TodoTile mandiri

--- 03-week-3-navigation-state-management/week3_todo/lib/pages/todo_page.dart
+++ 03-week-3-navigation-state-management/week3_todo/lib/pages/todo_page.dart
@@ -16,26 +16,8 @@ class TodoPage extends ConsumerWidget {
           ? const Center(child: Text('Belum ada tugas'))
           : ListView.builder(
               itemCount: todos.length,
-              itemBuilder: (context, index) => ListTile(
-                leading: Checkbox(
-                  value: todos[index].done,
-                  onChanged: (_) =>
-                      ref.read(todoListProvider.notifier).toggle(index),
-                ),
-                title: Text(
-                  todos[index].title,
-                  style: TextStyle(
-                    decoration: todos[index].done
-                        ? TextDecoration.lineThrough
-                        : null,
-                  ),
-                ),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () =>
-                      ref.read(todoListProvider.notifier).remove(index),
-                ),
-              ),
+              itemBuilder: (context, index) =>
+                  TodoTile(todo: todos[index], index: index),
             ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showAddDialog(context, ref),
@@ -66,6 +48,33 @@ class TodoPage extends ConsumerWidget {
             child: const Text('Tambah'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TodoTile extends ConsumerWidget {
+  final Todo todo;
+  final int index;
+
+  const TodoTile({super.key, required this.todo, required this.index});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      leading: Checkbox(
+        value: todo.done,
+        onChanged: (_) => ref.read(todoListProvider.notifier).toggle(index),
+      ),
+      title: Text(
+        todo.title,
+        style: TextStyle(
+          decoration: todo.done ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () => ref.read(todoListProvider.notifier).remove(index),
       ),
     );
   }
